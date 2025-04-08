6c78866db29c5693f0226ef7768b3728751b6fc8
Bartha Levente <b.levi.levi@gmail.com>
2025-04-08 08:52:36 +0200

    Kezdés

---
+++ src/main/java/com/example/MainController.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainController {
+
+    @FXML
+    void onClickShowButton(ActionEvent event) {
+        showDialog();
+
+    }
+
+    void showDialog() {
+        try {
+            tryshowDialog();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+
+        }
+    }
+
+    void tryshowDialog() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("dialogScene.fxml"));
+        Scene scene = new Scene(loader.load());
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+
+    }
+}
